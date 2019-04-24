aff5e63be19630f62b5380888efebd15ee696672
Vidy <vidy.thatte@gmail.com>
2019-04-23 17:28:29 -0700

    setup csv reader

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
+import CSVReader from "react-csv-reader";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+
+
+    this.handleForce = this.handleForce.bind(this);
+  
+  }
+
+  handleForce(data){
+    console.log(data);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <CSVReader
+            cssClass="react-csv-input"
+            label="Select CSV with secret Death Star statistics"
+            onFileLoaded={this.handleForce}
+          />
+          <p>and then open the console</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
