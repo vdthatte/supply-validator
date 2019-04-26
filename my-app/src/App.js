887180abdb2dc34ae50a32d3631b47d4f903d2cf
Vidy <vidy.thatte@gmail.com>
2019-04-26 13:04:01 -0700

    made relevant changes and improvements as requested

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -21,21 +21,19 @@ class App extends Component {
       approvedItems: 0,
 
 
-      image_url: "",
+      image: "",
       description: "",
       store_id: "",
       store_name: "",
       item_id: "",
-      item_name: "",
-      item_brand: "",
-      item_size: "",
+      listing_name: "",
+      brand: "",
+      size: "",
+      sku: "",
+      price: "",
+      approved: false,
 
-      csvData : [
-        ["firstname", "lastname", "email"],
-        ["Ahmed", "Tomi", "ah@smthing.co.com"],
-        ["Raed", "Labes", "rl@smthing.co.com"],
-        ["Yezzi", "Min l3b", "ymin@cocococo.com"]
-      ]
+      csvData : []
     
 
     }
@@ -57,9 +55,11 @@ class App extends Component {
     reader.addEventListener("loadend", function() {
         var data = reader.result;
         var json = JSON.parse(data);
+
         for(var i=0; i<json.length; i++){
           var new_json = json[i];
           new_json["approved"] = false
+          new_json["price"] = ""
           items.push(new_json);
         }
 
@@ -67,14 +67,15 @@ class App extends Component {
           isDataPresent: true,
           items: items,
 
-          image_url: items[0].image,
+          image: items[0].image,
           description: items[0].description,
           store_id: items[0].store_id,
           store_name: items[0].store_name,
           item_id: items[0].item_id,
-          item_name: items[0].listing_name,
-          item_brand: items[0].brand,
-          item_size: items[0].size,
+          listing_name: items[0].listing_name,
+          brand: items[0].brand,
+          size: items[0].size,
+          approved: items[0].approved
 
         })
 
@@ -96,31 +97,35 @@ class App extends Component {
     } else if(e.target.name == "approve"){
 
       var new_items = this.state.items
-      new_items[this.state.index]["image_url"] = this.state.image_url
+      new_items[this.state.index]["image"] = this.state.image
       new_items[this.state.index]["description"] = this.state.description
       new_items[this.state.index]["store_id"] = this.state.store_id
       new_items[this.state.index]["store_name"] = this.state.store_name
       new_items[this.state.index]["item_id"] = this.state.item_id
-      new_items[this.state.index]["item_name"] = this.state.item_name
-      new_items[this.state.index]["brand"] = this.state.item_brand
-      new_items[this.state.index]["size"] = this.state.item_size
+      new_items[this.state.index]["listing_name"] = this.state.listing_name
+      new_items[this.state.index]["brand"] = this.state.brand
+      new_items[this.state.index]["size"] = this.state.size
       new_items[this.state.index]["approved"] = true
+      new_items[this.state.index]["price"] = this.state.price
 
       this.setState({
         items: new_items,
-        approvedItems: this.state.approvedItems + 1
+        approvedItems: this.state.approvedItems + 1,
+        approved: true
       })
+
+      this.nextTapped()
 
     }
   }
 
   download(){
     var csv = []
-    var head_line = ["image_url","description", "store_id", "store_name", "item_id", "item_name", "brand", "size"]
+    var head_line = ["store_id", "store_name", "item_id", "listing_name", "brand", "description", "size", "sku", "image", "price"]
     csv.push(head_line)
     for(var i=0; i<this.state.items.length; i++){
       if(this.state.items[i]["approved"]){
-        var line = [this.state.items[i]["image_url"], this.state.items[i]["description"], this.state.items[i]["store_id"], this.state.items[i]["store_name"], this.state.items[i]["item_id"], this.state.items[i]["item_name"], this.state.items[i]["brand"], this.state.items[i]["size"]]
+        var line = [this.state.items[i]["store_id"], this.state.items[i]["store_name"], this.state.items[i]["item_id"], this.state.items[i]["listing_name"], this.state.items[i]["brand"], this.state.items[i]["description"], this.state.items[i]["size"], this.state.items[i]["sku"], this.state.items[i]["image"], this.state.items[i]["price"]]
         csv.push(line)
       }
     }
@@ -135,14 +140,16 @@ class App extends Component {
       var newIndex = this.state.index - 1
       this.setState({
         index : newIndex,
-        image_url: this.state.items[newIndex].image,
+        image: this.state.items[newIndex].image,
         description: this.state.items[newIndex].description,
         store_id: this.state.items[newIndex].store_id,
         store_name: this.state.items[newIndex].store_name,
         item_id: this.state.items[newIndex].item_id,
-        item_name: this.state.items[newIndex].listing_name,
-        item_brand: this.state.items[newIndex].brand,
-        item_size: this.state.items[newIndex].size
+        listing_name: this.state.items[newIndex].listing_name,
+        brand: this.state.items[newIndex].brand,
+        size: this.state.items[newIndex].size,
+        price: this.state.items[newIndex].price,
+        approved: this.state.items[newIndex].approved
       })
     }
   }
@@ -152,14 +159,16 @@ class App extends Component {
       var newIndex = this.state.index + 1
       this.setState({
         index : newIndex,
-        image_url: this.state.items[newIndex].image,
+        image: this.state.items[newIndex].image,
         description: this.state.items[newIndex].description,
         store_id: this.state.items[newIndex].store_id,
         store_name: this.state.items[newIndex].store_name,
         item_id: this.state.items[newIndex].item_id,
-        item_name: this.state.items[newIndex].listing_name,
-        item_brand: this.state.items[newIndex].brand,
-        item_size: this.state.items[newIndex].size
+        listing_name: this.state.items[newIndex].listing_name,
+        brand: this.state.items[newIndex].brand,
+        size: this.state.items[newIndex].size,
+        price: this.state.items[newIndex].price,
+        approved: this.state.items[newIndex].approved
       })
     }
   }
@@ -177,7 +186,7 @@ class App extends Component {
           !this.state.isDataPresent ? (
             <div className="uploadBtn">
               <ReactFileReader handleFiles={this.handleFiles} fileTypes={[".json"]}>
-                <button className='btn'>Upload</button>
+                <button className='btn'>Upload JSON file</button>
               </ReactFileReader>
             </div>
           ) : (
@@ -185,8 +194,8 @@ class App extends Component {
                 <div className="item">
                 <div className="row">
                   <div className="col">
-                    Image: <input className="item_image_url" value={this.state.image_url} name="image_url" onChange={this.handleInputChange}/> 
-                    <img className="item_image" src={this.state.image_url} name="image_url" width="250" height="250" onChange={this.handleInputChange}/> <br/>
+                    Image: <input className="item_image_url" value={this.state.image} name="image" onChange={this.handleInputChange}/> 
+                    <img className="item_image" src={this.state.image} name="image" width="250" height="250" onChange={this.handleInputChange}/> <br/>
                     Description:
                     <p> <textarea className="item_description" name="description" value={this.state.description} rows="5" onChange={this.handleInputChange}/></p>
                   </div>
@@ -195,19 +204,31 @@ class App extends Component {
                     <p>Store Id: <input className="item_store_id" name="store_id" value={this.state.store_id} onChange={this.handleInputChange}/></p> 
                     <p>Store Name: <input className="item_store_name" name="store_name" value={this.state.store_name} onChange={this.handleInputChange}/></p> 
                     <p>ID: <input className="item_id" name="item_id" value={this.state.item_id} onChange={this.handleInputChange}/></p> 
-                    <p>Name: <input className="item_name" name="item_name" value={this.state.item_name} onChange={this.handleInputChange}/> </p>
-                    <p>Brand: <input className="item_brand" name="item_brand" value={this.state.item_brand} onChange={this.handleInputChange}/> </p>
-                    <p>Size: <input className="item_size" name="item_size" value={this.state.item_size} onChange={this.handleInputChange}/></p> 
+                    <p>Name: <input className="item_name" name="listing_name" value={this.state.listing_name} onChange={this.handleInputChange}/> </p>
+                    <p>Brand: <input className="item_brand" name="brand" value={this.state.brand} onChange={this.handleInputChange}/> </p>
+                    <p>Size: <input className="item_size" name="size" value={this.state.size} onChange={this.handleInputChange}/></p> 
+                    <p>Price: <input className="item_size" name="price" value={this.state.price} onChange={this.handleInputChange}/></p> 
                   </div>
                 </div>
 
-                <div className="downloadSection"><button onClick={this.download}>Download</button></div>
+                <div className="downloadSection">
+                  <button className="deleteButton" onClick={this.buttonClick} value="remove" name="remove">Delete</button>
+                  <button className="downloadButton" onClick={this.download}>Download {this.state.approvedItems} items</button>
+                  {
+                    (!this.state.approved) ? (
+                      <button className="approveButton" onClick={this.buttonClick} value="approve" name="approve">Approve</button>
+                    ) : (
+                      <button className="approveButton" value="approve" name="approve">Approved</button>
+                    )
+                  }
+                  
                 </div>
-                {/* <button key={this.state.index} value={this.state.index} name="remove" onClick={this.handleClick}>Remove</button><button value={this.state.index} key={this.state.index}  onClick={this.handleClick}>Approve</button> */}
-                <button className="approveButton" onClick={this.buttonClick} value="remove" name="remove">Delete</button>
-                <button className="nextbutton" onClick={this.prevTapped}><img width="10" height="20" src={process.env.PUBLIC_URL + '/left.png'} /></button>{this.state.index} of {this.state.items.length} items
+
+                </div>
+                
+                <button className="prevbutton" onClick={this.prevTapped}><img width="10" height="20" src={process.env.PUBLIC_URL + '/left.png'} /></button>{this.state.index} of {this.state.items.length} items
                 <button className="nextbutton" onClick={this.nextTapped}><img width="10" height="20" src={process.env.PUBLIC_URL + '/right.png'} /></button>
-                <button className="approveButton" onClick={this.buttonClick} value="approve" name="approve">Approve</button>
+                
                 <br/>
                 {
                   (this.state.isDownloadTapped)?(<CSVDownload data={this.state.csvData} target="_blank" />):(null)
